cc828a5b2532fee9d6117321894a9c19931a5f56
iamngoni <imngonii@gmail.com>
2022-09-20 19:27:16 +0200

    Initial Stuff

---
+++ lib/app/view/landing.dart
@@ -0,0 +1,17 @@
+/*
+    @Project: modestnerd
+    @Date: 20, September, 2022
+    @Author: Ngonidzashe Mangudya
+ 
+    Copyright (c) 2022 ModestNerd
+*/
+import 'package:flutter/material.dart';
+
+class Landing extends StatelessWidget {
+  const Landing({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
